288dddb8039c2a7470fbc0505a77937dd12a51d3
Adam Nowak <nowakadam1980@gmail.com>
2026-05-11 18:55:47 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  async redirects() {
+    return [
+      {
+        source: '/sitemap',
+        destination: '/sitemap.xml',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 const withMDX = createMDX({
